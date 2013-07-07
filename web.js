64d14451ae601e8825068db7f665fa6d527abc02
Ashley Bennett <prospero168@aol.com>
2013-07-07 17:49:36 -0400

    Update web.js

--- web.js
+++ web.js
@@ -3,9 +3,6 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('index.html');
-  
-var buffer = new Buffer("Hello World from index.html", 25);
  fs.readFileSync("index.html", "utf-8");
 });
 
