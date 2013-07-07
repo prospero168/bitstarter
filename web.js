0ce1c57c12bd08489787561bb2e4cd5f2b95eff2
Ashley Bennett <prospero168@aol.com>
2013-07-07 18:21:24 -0400

    Update web.js

--- web.js
+++ web.js
@@ -4,6 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
  fs.readFileSync("index.html", "utf-8");
+ response.send(resultOfreadFileSync);
 });
 
 var port = process.env.PORT || 5000;
